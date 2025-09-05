70a0350a4f4d5a37f42b75308b9e77c7540fa5f1
Rutuja Lambat <144705711+Rutujaajaylamabt@users.noreply.github.com>
2025-09-05 12:25:22 +0530

    Updated App.js to use HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light');
